30f26f38a299109df689520b45eb4678e7a15458
Engin Gedik <enggdk13@gmail.com>
2023-12-03 01:13:39 +0300

    get all and get by id

--- FastAPI/udemy_course/own_code/adapted_code/games2.py
+++ FastAPI/udemy_course/own_code/adapted_code/games2.py
@@ -34,38 +34,52 @@ class GameRequest(BaseModel):
                 "game_name": "game name",
                 "game_dev": "studio that developed the game",
                 "game_desc": "a brief description of the game",
-                "game_rating": "3",
-                "game_published": "2000",
-                "game_price": "10"
+                "game_rating": 3,
+                "game_published": 2000,
+                "game_price": 10
             }
         }
 
 #GAMES are a list of elements that are structured by the Game class
 GAMES = [
-    Game(1, "Fallout 4", "Bethesda", "a survival game", "4", "2015", "60"),
-    Game(2, "The Elder Scrolls V: Skyrim", "Bethesda", "an open-world RPG", "5", "2011", "40"),
-    Game(3, "Fallout: New Vegas", "Obsidian Entertainment", "an RPG", "3", "2010", "30"),
-    Game(4, "The Witcher 3: Wild Hunt", "CD Projekt", "an action RPG", "0", "2015", "50"),
-    Game(5, "Far Cry 4", "Ubisoft", "a first-person shooter", "1", "2014", "45"),
-    Game(6, "Mass Effect 2", "BioWare", "an action RPG", "3", "2010", "35"),
-    Game(7, "Red Dead Redemption 2", "Rockstar Games", "an action-adventure", "5", "2018", "70"),
-    Game(8, "Dark Souls III", "FromSoftware", "an action RPG", "5", "2016", "55"),
-    Game(9, "Borderlands 2", "Gearbox Software", "a first-person shooter", "4", "2012", "40"),
-    Game(10, "Doom (2016)", "id Software", "a first-person shooter", "0", "2016", "50"),
-    Game(11, "BioShock Infinite", "Irrational Games", "a first-person shooter", "5", "2013", "45"),
-    Game(12, "The Last of Us", "Naughty Dog", "an action-adventure", "2", "2013", "60"),
-    Game(13, "Dragon Age: Inquisition", "BioWare", "an action RPG", "1", "2014", "55"),
-    Game(14, "Star Wars Jedi: Fallen Order", "Respawn Entertainment", "an action-adventure", "4", "2019", "65"),
-    Game(15, "Metro Exodus", "4A Games", "a first-person shooter", "3", "2019", "50"),
-    Game(16, "Assassin's Creed Odyssey", "Ubisoft", "an action RPG", "3", "2018", "55"),
-    Game(17, "Divinity: Original Sin 2", "Larian Studios", "a role-playing game", "5", "2017", "60"),
-    Game(18, "Fortnite", "Epic Games", "a battle royale", 2, "2017", "0"),
-    Game(19, "Minecraft", "Mojang", "a sandbox", 5, "2011", "30"),
-    Game(20, "RuneScape", "Jagex", "a MMORPG", 1, "2001", "0"),
-    Game(21, "ARK: Survival Evolved", "Studio Wildcard", "a survival game", 1, "2017", "50"),
-    Game(22, "Civilization V", "Firaxis Games", "a strategy game", 5, "2010", "40"),
-    Game(23, "Civilization VI", "Firaxis Games", "a strategy game", 2, "2016", "60"),
-    Game(24, "Apex Legends", "Respawn Entertainment", "a battle royale", 5, "2019", "0"),
-    Game(25, "Genshin Impact", "miHoYo", "an action RPG", 3, "2020", "0")
+    Game(1, "Fallout 4", "Bethesda", "a survival game", 4, 2015, 60),
+    Game(2, "The Elder Scrolls V: Skyrim", "Bethesda", "an open-world RPG", 5, 2011, 40),
+    Game(3, "Fallout: New Vegas", "Obsidian Entertainment", "an RPG", 3, 2010, 30),
+    Game(4, "The Witcher 3: Wild Hunt", "CD Projekt", "an action RPG", 0, 2015, 50),
+    Game(5, "Far Cry 4", "Ubisoft", "a first-person shooter", 1, 2014, 45),
+    Game(6, "Mass Effect 2", "BioWare", "an action RPG", 3, 2010, 35),
+    Game(7, "Red Dead Redemption 2", "Rockstar Games", "an action-adventure", 5, 2018, 70),
+    Game(8, "Dark Souls III", "FromSoftware", "an action RPG", 5, 2016, 55),
+    Game(9, "Borderlands 2", "Gearbox Software", "a first-person shooter", 4, 2012, 40),
+    Game(10, "Doom (2016)", "id Software", "a first-person shooter", 0, 2016, 50),
+    Game(11, "BioShock Infinite", "Irrational Games", "a first-person shooter", 5, 2013, 45),
+    Game(12, "The Last of Us", "Naughty Dog", "an action-adventure", 2, 2013, 60),
+    Game(13, "Dragon Age: Inquisition", "BioWare", "an action RPG", 1, 2014, 55),
+    Game(14, "Star Wars Jedi: Fallen Order", "Respawn Entertainment", "an action-adventure", 4, 2019, 65),
+    Game(15, "Metro Exodus", "4A Games", "a first-person shooter", 3, 2019, 50),
+    Game(16, "Assassin's Creed Odyssey", "Ubisoft", "an action RPG", 3, 2018, 55),
+    Game(17, "Divinity: Original Sin 2", "Larian Studios", "a role-playing game", 5, 2017, 60),
+    Game(18, "Fortnite", "Epic Games", "a battle royale", 2, 2017, 0),
+    Game(19, "Minecraft", "Mojang", "a sandbox", 5, 2011, 30),
+    Game(20, "RuneScape", "Jagex", "a MMORPG", 1, 2001, 0),
+    Game(21, "ARK: Survival Evolved", "Studio Wildcard", "a survival game", 1, 2017, 50),
+    Game(22, "Civilization V", "Firaxis Games", "a strategy game", 5, 2010, 40),
+    Game(23, "Civilization VI", "Firaxis Games", "a strategy game", 2, 2016, 60),
+    Game(24, "Apex Legends", "Respawn Entertainment", "a battle royale", 5, 2019, 0),
+    Game(25, "Genshin Impact", "miHoYo", "an action RPG", 3, 2020, 0)
 ]
 
+
+# gets all books
+@app.get("/GAME/", status_code=status.HTTP_200_OK)
+async def read_all_games():
+    return GAMES
+
+
+# gets games based on id input
+@app.get("/GAME/{game_id}", status_code=status.HTTP_200_OK)
+async def read_game_by_id(game_id: int):
+    for game in GAMES:
+        if game.game_id == game_id:
+            return game
+    raise HTTPException(status_code=404, detail="item not found")
