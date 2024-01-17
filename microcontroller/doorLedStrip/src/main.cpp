c6e4b74e27a0f60d033123e1cb23962064d719ad
Engin Gedik <enggdk13@gmail.com>
2024-01-17 17:31:36 +0300

    reformatting files and dependencies

--- microcontroller/doorLedStrip/src/main.cpp
+++ microcontroller/doorLedStrip/src/main.cpp
@@ -1,18 +1,43 @@
+/// @file    Blink.ino
+/// @brief   Blink the first LED of an LED strip
+/// @example Blink.ino
+
 #include <Arduino.h>
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <FastLED.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// number of leds in a strip
+#define NUM_LEDS 6
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+#define DATA_PIN 3
+#define CLOCK_PIN 13
+
+// Define the array of leds
+CRGB leds[NUM_LEDS];
+
+void setup()
+{
+    FastLED.addLeds<NEOPIXEL, DATA_PIN, RGB>(leds, NUM_LEDS); // RGB ordering
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
+    // Turn the LED on, then pause
+    // refers to the led at point
+    leds[0] = CRGB(0, 255, 0);
+    delay(3000);
+    FastLED.show();
+    leds[1] = CRGB(255, 0, 0);
+    delay(3000);
+    FastLED.show();
+    leds[2] = CRGB(0, 0, 255);
+    delay(3000);
+    FastLED.show();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    // Now turn the LED off, then pause
+    // leds[0] = CRGB::Black;
+    // FastLED.show();
+    // delay(500);
 }
