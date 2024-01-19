3825dd46d1fc2f25a5d79fc9875239efb7b8a5cc
Engin Gedik <enggdk13@gmail.com>
2024-01-19 09:38:49 +0300

    base file for api building for the strip

---
+++ microcontroller/doorLedStrip/src/no_webUI.cpp
@@ -0,0 +1,31 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <FastLED.h>
+
+// Replace with your network credentials
+const char *ssid = "MW42V_A8CA";
+const char *password = "91014264";
+
+String sliderValueRed = "0";
+String sliderValueGreen = "0";
+String sliderValueBlue = "0";
+
+#include <Arduino.h>
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <FastLED.h>
+
+// Replace with your network credentials
+const char *ssid = "MW42V_A8CA";
+const char *password = "91014264";
+
+void setup()
+{
+}
+
+void loop()
+{
+}
