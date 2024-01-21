160025ca0ea6d43a4d6b09d75574dbe6a2937911
Engin Gedik <enggdk13@gmail.com>
2024-01-21 10:46:06 +0300

    file refactoring

--- microcontroller/doorLedStrip/eepromHSV.cpp
+++
@@ -1,9 +0,0 @@
-struct LedStruct
-{
-    int ledID;      // 0-5 led ID
-    int fooID;      // 0-4 function to be routed to
-    int hue;        // 0-255 range for hue
-    int saturation; // 0-255 range for saturation
-    int brightness; // 0-255 range for brightness
-    int fooMod;     // modifier for blink interval, auto shutdown time, breathe speed
-};
