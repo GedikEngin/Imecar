f6ef9b3da7e66b842bae1ada4875f76eb3867df3
Engin Gedik <enggdk13@gmail.com>
2024-02-28 09:16:31 +0300

    server created links db&routes&controllers&validators

--- meetingRoom4/server.js
+++ meetingRoom4/server.js
@@ -1,23 +1,28 @@
-// imports
 const express = require("express");
 const userRouter = require("./routers/userRouter");
 const roomRouter = require("./routers/roomRouter");
 const meetingRouter = require("./routers/meetingRouter");
+const { connect } = require("./configs/dbConfig"); // Import the connect function
 require("dotenv").config();
 
-// instantiation
 const app = express();
 const port = process.env.PORT || 8080;
 
-// Use middleware for parsing JSON requests
 app.use(express.json());
 
-// Use routers
 app.use("/user", userRouter);
 app.use("/room", roomRouter);
 app.use("/meeting", meetingRouter);
 
-// Start server
-app.listen(port, () => {
-	console.log(`Server is running on port ${port}`);
-});
+// Connect to the database when the server starts
+connect()
+	.then(() => {
+		// Start the server after establishing the database connection
+		app.listen(port, () => {
+			console.log(`Server is running on port ${port}`);
+		});
+	})
+	.catch((error) => {
+		console.error("Error connecting to the database:", error);
+		process.exit(1); // Exit with error
+	});
