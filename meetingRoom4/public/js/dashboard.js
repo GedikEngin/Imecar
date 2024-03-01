3796a00b1e04242005a95f4feb20d42355fbe779
Engin Gedik <enggdk13@gmail.com>
2024-03-01 04:43:08 +0300

    removed calender urls as it got merged

--- meetingRoom4/public/js/dashboard.js
+++ meetingRoom4/public/js/dashboard.js
@@ -2,22 +2,15 @@
 
 // Get references to the buttons
 const meetingsBtn = document.getElementById("meetingsBtn");
-const calendarBtn = document.getElementById("calendarBtn");
 const roomsBtn = document.getElementById("roomsBtn");
 
 // Add event listeners to the buttons
 meetingsBtn.addEventListener("click", redirectToMeetings);
-calendarBtn.addEventListener("click", redirectToCalendar);
 roomsBtn.addEventListener("click", redirectToRooms);
 
 // Function to redirect to the meetings page
 function redirectToMeetings() {
 	window.location.href = "../html/meetings.html"; // Change the URL to the actual route
-}
-
-// Function to redirect to the calendar page
-function redirectToCalendar() {
-	window.location.href = "../html/calendar.html"; // Change the URL to the actual route
 }
 
 // Function to redirect to the rooms page
