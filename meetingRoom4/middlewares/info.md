7eeaa7a0ba3b1cd75d9eb31bc397306cbf8876a7
Engin Gedik <enggdk13@gmail.com>
2024-03-03 21:34:26 +0300

    file and folder clean up

--- meetingRoom4/middlewares/info.md
+++ meetingRoom4/middlewares/info.md
@@ -1,1 +1,2 @@
 contains middleware, such as data validation (although that has been moved to the models themselves instead of using express-validator)
+has responses and dtos to format responses and data being passed through the system
--- meetingRoom4/old/test.js
+++ meetingRoom4/old/test.js
@@ -1,8 +1,8 @@
 // Require necessary modules
-const { sequelize } = require("./configs/dbConfig"); // Adjust the path as needed
-const { Meeting } = require("./models/meetingModel"); // Adjust the paths as needed
-const { Room } = require("./models/roomModel"); // Adjust the paths as needed
-const { User } = require("./models/userModel"); // Adjust the paths as needed
+const { sequelize } = require("../configs/dbConfig"); // Adjust the path as needed
+const { Meeting } = require("../models/meetingModel"); // Adjust the paths as needed
+const { Room } = require("../models/roomModel"); // Adjust the paths as needed
+const { User } = require("../models/userModel"); // Adjust the paths as needed
 
 // Custom Sync function to synchronize models with the database
 async function syncDatabase() {
